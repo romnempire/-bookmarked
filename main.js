9bdb47241f2ca82b03adf74259f28c80bbe744e9
Chris Bradfield <chriswbrad@gmail.com>
2015-01-17 17:17:17 -0500

    Works as extension

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-function getBookmarks(){
+function importBookmarks(){
 	var currentList = [];
 	list = " ";
 	chrome.bookmarks.getTree(function(itemTree){
@@ -16,40 +16,48 @@ function getBookmarks(){
 	    }
 
 	    if(node.url){
-			currentNode = {'id':1, 'url':node.url, 'Name':node.title};
+			currentNode = {'id':1, 'url':node.url, 'Name':node.title, "tags":['a','b','c']};
 		}
 	}
 }
 
 function addBookmark(){
+	tagString = $("#newTags").val();
+
+	tags = tagString.split(",");
+	tags = tags.map(function(s) { return s.trim() });
+
 	chrome.tabs.getSelected(null,function(tab){
 		myURL=tab.url;
 		myTitle = tab.title;
 		chrome.storage.sync.get('bookmarks',function(test){
 			if(test['bookmarks']!=null){
-				chrome.storage.sync.set({'bookmarks': test['bookmarks'] + JSON.stringify({'id':1,'url':myURL, 'Name':myTitle})}, function(result) {
+				chrome.storage.sync.set({'bookmarks': test['bookmarks'] + JSON.stringify({'id':1,'url':myURL, 'Name':myTitle, "tags":tags})}, function(result) {
+					alert("Bookmark added");
 				});
 			} else {
-				chrome.storage.sync.set({'bookmarks': JSON.stringify({'id':1, 'url':myURL, 'Name':myTitle})}, function(result) {
+				chrome.storage.sync.set({'bookmarks': JSON.stringify({'id':1, 'url':myURL, 'Name':myTitle, "tags":tags})}, function(result) {
+					alert("Bookmark added");
 				});
 			}
-			alert(test['bookmarks']);
 		});
 
 	});
+}
 
+function getBookmarks(){
+	var retValue;
+	alert(chrome.storage.sync.get('bookmarks',function(test){
+		alert(test['bookmarks']);
+		return "EHW";
+	}));
 }
 
 function clearBookmarks(){
-	chrome.storage.sync.remove('bookmarks',function(){alert("REMOVED");})
+	chrome.storage.sync.remove('bookmarks',function(){alert("Bookmarks Cleared");})
 }
 
 
 document.getElementById("addBookmark").addEventListener("click",function(){addBookmark();});
 
 document.getElementById("clearBookmarks").addEventListener("click",function(){clearBookmarks();});
-
-
-window.addEventListener('load', function () {
-
-});
---
+++ tagview.js
@@ -0,0 +1,100 @@
+var onTags = [];
+//tagcounts is an array of arrays.  the arrays hold the ids of images with a tag
+//the array size is the weight of the tag.
+var tagCounts = [];
+var data = {
+	'0001': {
+		'id': '0001',
+		'name': 'Google',
+		'url': 'http://google.com',
+		'tags': ['awesome', 'search', 'bookmark']
+	},
+	'0002': {
+		'id': '0002',
+		'name': 'Steam',
+		'url': 'http://store.steampowered.com',
+		'tags': ['money', 'pls', 'bookmark']
+	},
+	'0003': {
+		'id': '0003',
+		'name': 'DotA',
+		'url': 'www.dota2.com',
+		'tags': ['halp', 'bookmark']
+	},
+	'0004': {
+		'id': '0004',
+		'name': 'Reddit',
+		'url': 'http://www.reddit.com',
+		'tags': ['bongo', 'bookmark']
+	},
+	'0005': {
+		'id': '0005',
+		'name': 'Netflix',
+		'url': 'http://www.netflix.com',
+		'tags': ['bookmark']
+	}
+
+};
+
+var populateTags = function populateTags() {
+
+	for(bookmark in data) {
+		for(tag in data[bookmark].tags) {
+			if(tagCounts[data[bookmark].tags[tag]]) {
+				tagCounts[data[bookmark].tags[tag]].push(bookmark);
+			} else {
+				tagCounts[data[bookmark].tags[tag]] = [bookmark];
+			}
+
+		}
+	}
+	for(tag in tagCounts) {
+		console.log(tagCounts);
+		var styleLine = "display: inline; padding: 5px 5px 5px 5px;";
+		//sizes tags to account for popularity
+		if (tagCounts[tag].length >= 100) {
+			styleLine += "font-size: 4em";
+		} else if (tagCounts[tag].length >= 10) {
+			styleLine += "font-size: 2em";
+		} else if (tagCounts[tag].length >= 5) {
+			styleLine += "font-size: 1.5em";;
+		}  else if (tagCounts[tag].length >= 5) {
+			styleLine += "font-size: 1.25em";
+		}
+		$('#tagmap').append('<div id="'+ tag +'" style="'+ styleLine+'">#'+tag+'</div>');
+		$("#"+tag).click(function(){toggleTag(this.id)});
+	}
+};
+
+var toggleTag = function toggleTag(tag) {
+	if(onTags.indexOf(tag) == -1) {
+		onTags.push(tag);
+		$('#'+tag).css('background-color','lightgreen');
+	} else {
+		onTags.splice(onTags.indexOf(tag), 1);
+		$('#'+tag).css('background-color','white');
+	}
+};
+
+var gibWebpage = function gibWebpage() {
+	var webpages = [];
+	for (tag in onTags) {
+		webpages = webpages.concat(tagCounts[onTags[tag]]);
+	}
+
+	//pick a random element from webpages
+	var ele = webpages[Math.floor((Math.random() * webpages.length))];
+	var site = data[ele].url;
+	window.location = site;
+}
+
+
+$('document').ready(function() {
+
+	document.getElementById("feelingLucky").addEventListener("click",function(){
+		gibWebpage();
+	});
+
+	populateTags();
+
+});
